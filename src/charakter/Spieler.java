805bd3a7a0598e8380d173f1bf2a29637f2e8fd5
Scb91 <siebo.bortz@outlook.de>
2025-10-09 14:51:53 +0200

    floats in int  umgewandelt, methoden in spieler hinzugefügt und testklasse (#4)

--- src/charakter/Spieler.java
+++ src/charakter/Spieler.java
@@ -1,12 +1,13 @@
-//Unterklasse Spieler erbt von Charakter und erweitert für
+package charakter;
+//Unterklasse Spieler erbt von Charakter und erweitert für den Spieler notwendige Dinge
 public class Spieler extends Charakter{
     private int aktionspunkte;
     private int maxAktionspunkte;
     private int aktErfahrungspunkte;
     private int benErfahrungspunkte;
-    
+    private int steigerungsWert = 20;
 //Konstruktor der Unterklasse
-    public Spieler(String name, float maxLebenspunkte, float angriffsWert, int level) {
+    public Spieler(String name, int maxLebenspunkte, int angriffsWert, int level) {
         super(name, maxLebenspunkte, angriffsWert, level);
         this.maxAktionspunkte = 1;
         this.aktionspunkte = maxAktionspunkte;
@@ -16,6 +17,10 @@ public class Spieler extends Charakter{
 //Setter für Variablen der Unterklasse
     public void setAktionspunkte(int aktionspunkte) {
         this.aktionspunkte = aktionspunkte;
+    }
+
+    public void setmaxAktionspunkte(int maxAktionspunkte) {
+        this.maxAktionspunkte = maxAktionspunkte;
     }
 
     public void setAktErfahrungspunkte(int aktErfahrungspunkte) {
@@ -30,6 +35,10 @@ public class Spieler extends Charakter{
         return this.aktionspunkte;
     }
 
+    public int getmaxAktionspunkte() {
+        return this.maxAktionspunkte;
+    }
+
     public int getAktErfahrungspunkte() {
         return this.aktErfahrungspunkte;
     }
@@ -38,35 +47,42 @@ public class Spieler extends Charakter{
         return this.benErfahrungspunkte;
     }
 //Methoden der Unterklasse
-    public void aufleveln(boolean levelup){
-        if (levelup){
-            float plusLebenspunkte = (float) (this.getmaxLebenspunkte()*0.2);
-            if ((this.getmaxLebenspunkte()*0.2)%1!=0){
-                plusLebenspunkte = plusLebenspunkte-((float) (this.getmaxLebenspunkte()*0.2)%1)+1;
-            }
-            this.setLevel(this.getLevel()+1);
-            this.setBenErfahrungspunkte(getBenErfahrungspunkte()*2);
-            this.setmaxLebenspunkte(this.getmaxLebenspunkte()+plusLebenspunkte);
-            this.setaktLebenspunkte(this.getaktLebenspunkte()+plusLebenspunkte);
-            if (((this.getAngriffsWert()*1.2)%1)!=0){
-                this.setangriffsWert((float) ((this.getAngriffsWert()*1.2)-((this.getAngriffsWert()*1.2)%1)+1));
-            }
-            else {
-                this.setangriffsWert((float) ((this.getAngriffsWert()*1.2)));
-            }
-        }
+    //Methode was passiert wenn der Spieler ein Level aufsteigt
+    public void aufleveln(){
+        this.setLevel(this.getLevel()+1);
+        this.setBenErfahrungspunkte(getBenErfahrungspunkte()*2);
+        lebenspunkteSteigern();
+        angriffSteigern();
     }
-
+    //Methode für die Verarbeitung von Erfahrungspunkten
     public void bekommeErfahrung(int erfWert){
         if ((erfWert+this.aktErfahrungspunkte)>=this.benErfahrungspunkte){
             this.setAktErfahrungspunkte(erfWert+this.aktErfahrungspunkte);
             do {
                 this.setAktErfahrungspunkte((this.aktErfahrungspunkte)-this.benErfahrungspunkte);
-                this.aufleveln(true);}
+                this.aufleveln();}
                 while ((this.aktErfahrungspunkte)>=this.benErfahrungspunkte);
         }
         else {
             this.setAktErfahrungspunkte((erfWert+this.aktErfahrungspunkte));
         }
+    }
+    //Methode zur steigerung des Angriffs(wird bei "aufleveln()" aufgerufen)
+    public void angriffSteigern(){
+        int zwischenergebnis = this.getAngriffsWert()*steigerungsWert;
+        if (zwischenergebnis-(zwischenergebnis/100*100)>=50){
+            zwischenergebnis=zwischenergebnis+100;
+        }
+        this.setangriffsWert(this.getAngriffsWert()+zwischenergebnis/100);
+    }
+    //Methode zu Steigerung der Lebenspunkte(wird bei "aufleveln()" aufgerufen)
+    public void lebenspunkteSteigern(){
+        int zwischenergebnis = this.getmaxLebenspunkte()*steigerungsWert;
+        if (zwischenergebnis-(zwischenergebnis/100*100)>=50){
+            zwischenergebnis=zwischenergebnis+100;
+        }
+        int plusLebenspunkte = zwischenergebnis/100;
+        this.setmaxLebenspunkte(this.getmaxLebenspunkte()+plusLebenspunkte);
+        this.setaktLebenspunkte(this.getaktLebenspunkte()+plusLebenspunkte);
     }
 }
