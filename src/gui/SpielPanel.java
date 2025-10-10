89cdd6bc73c2130c0c968acb5e1b00190eb08027
oraclez <ababdu.b1994@gmail.com>
2025-10-10 12:17:13 +0200

    ZeigeKampffenster methode in Spielpanel

--- src/gui/SpielPanel.java
+++ src/gui/SpielPanel.java
@@ -8,24 +8,26 @@ import kampf.Einzelkampf;
 
 public class SpielPanel extends JPanel {
     private Image playerImage;
-
+    
+    
     public SpielPanel() {
         setLayout(null); // du benutzt aktuell absolutes Layout
-        playerImage = new ImageIcon(getClass().getResource("/assets/Warrior_Idle1.jpg")).getImage();
+        
+
+
+
+    }
+        public void zeigeKampfFenster(){
+        playerImage = new ImageIcon(getClass().getResource("/assets/bg.jpg")).getImage();
         Spieler sp = new Spieler("Oraclez", 100, 10, 1);
         Gegner gn = new Gegner("Orc", 70, 1, 1);
         Einzelkampf kampf = new Einzelkampf(sp, gn);
-
         JTextArea combatLogArea = new JTextArea();
         combatLogArea.setEditable(false);
         combatLogArea.setLineWrap(true);
         combatLogArea.setWrapStyleWord(true);
 
-        // Alle Buttons
-        // JButton btnStart = new JButton("Start Game");
-        // btnStart.setBounds(100, 175, 100, 25);
-        // add(btnStart);
-        // btnStart.addActionListener(e -> Einzelkampf.neuesSpiel(sp, gn));
+        
 
         JButton btnAngriff = new JButton("Angriff");
         btnAngriff.setBounds(100, 500, 100, 25);
@@ -47,21 +49,17 @@ public class SpielPanel extends JPanel {
         btnFaehigkeit.setBounds(100, 525, 100, 25);
         add(btnFaehigkeit);
         btnFaehigkeit.addActionListener(e -> {
-            kampf.faehigkeit();
-            ; // Aktion ausführen
+            kampf.faehigkeit();; // Aktion ausführen
             combatLogArea.append(kampf.getCombatLog());
             // Textfeld aktualisieren
         });
 
-        // JButton btnAufgeben = new JButton("Beenden");
-        // btnAufgeben.setBounds(100, 275, 100, 25);
-        // add(btnAufgeben);
 
         JLabel lblNamenAnzeige = new JLabel(sp.getName());
         lblNamenAnzeige.setBounds(275, 250, 50, 25);
         add(lblNamenAnzeige);
 
-        JLabel lblLvl = new JLabel("lvl " + sp.getLevel());
+        JLabel lblLvl = new JLabel("lvl "+sp.getLevel());
         lblLvl.setBounds(350, 250, 25, 25);
         add(lblLvl);
 
@@ -87,14 +85,15 @@ public class SpielPanel extends JPanel {
         scrollPane.setBounds(250, 500, 400, 150);
 
         add(scrollPane);
-
     }
+
+
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (playerImage != null) {
-            g.drawImage(playerImage, 300, 250, 128, 128, this);
+            g.drawImage(playerImage, 125, 250, 128, 128, this);
         }
     }
 }
